0812b9bcd2d8d33dd45eef2a8492833f5e2cf18d
Gary Cheng <garycheng@berkeley.edu>
2016-10-28 03:13:21 -0700

    made a basic version of program with markov chains

--- learn_rap_structures.py
+++ learn_rap_structures.py
@@ -3,6 +3,28 @@ from rgenius_vars import *
 from bs4 import BeautifulSoup
 import re
 
+def make_markov_dict():
+    song_to_lyrics = make_song_to_lyrics_dict()
+    markov_dict = {}
+    for song in song_to_lyrics:
+        lyrics = song_to_lyrics[song]
+        for line in lyrics:
+            for i_word in range(0, len(line)):
+                curWord = line[i_word]
+                nextWord = "\END"
+                if i_word + 1 < len(line):
+                    nextWord = line[i_word + 1]
+
+                if curWord not in markov_dict:
+                    markov_dict[curWord] = []
+                markov_dict[curWord].append(nextWord)
+
+    for word in markov_dict:
+        print word
+        print markov_dict[word]
+    #return markov_dict
+
+
 def make_song_to_lyrics_dict():
     #returns songs of kendrick lamar
     base_url = "https://api.genius.com"
@@ -23,7 +45,7 @@ def make_song_to_lyrics_dict():
         path = song_to_path[title]
         lyrics = scrape_and_clean_lyrics(path)
         song_to_lyrics[title] = lyrics
-    #return song_to_lyrics
+    return song_to_lyrics
 
 #TODO remove the lines that correspond to album tracks etc.
 def scrape_and_clean_lyrics(path):
@@ -46,10 +68,8 @@ def scrape_and_clean_lyrics(path):
         if is_valid_line(lyric):
             #remove parenthesis that are in the middle of the line
             lyric = re.sub(r"[()]+","", lyric)
+            lyric = lyric.split(" ")
             clean_lyrics.append(lyric)
-
-    for lyric in clean_lyrics:
-        print lyric
     return clean_lyrics
 
 def is_valid_line(lyric):
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datamuse import datamuse
-import requests
+from learn_rap_structures import *
+import random
 api = datamuse.Datamuse()
 
 def main():
@@ -7,18 +8,31 @@ def main():
     # parse args or something
 
     num_of_lines = 10
-    topic = "topic"
-    last_word = "word"
-    while num_of_lines:
-        sentence = make_sentence(topic, last_word)
-        last_word = sentence[-1:]
-        topic = new_topic(topic)
-        num_of_lines -= 1
+    rap = []
+    # topic = "topic"
+    # last_word = "word"
+    markov_dict = make_markov_dict()
+    for count in range(0, num_of_lines):
+        sentence = make_sentence(markov_dict)
+        rap.append(sentence)
+        #last_word = sentence[-1:]
+        #topic = new_topic(topic)
+
+    #print rap
+    for line in rap:
+        print line
 
 
 # returns a list of words that comprise a sentence
-def make_sentence(topic, end_rhyme, syllables = None):
-    returns
+def make_sentence(markov_dict, topic = None, end_rhyme = None, syllables = None):
+    sentence = ""
+    word = random.choice(markov_dict.keys())
+    count = 0
+    while word != "\END" and count < 10:
+        sentence += word
+        word = random.choice(markov_dict[word])
+        count += 1
+    return sentence
 
 def new_topic(old_topic):
     t = api.words(topics = 'old_topic', max = 10)
